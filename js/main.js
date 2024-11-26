1b8f250b4aea057d14c21c631ba2caf6fc3d08c9
Dana Gabay <danagabay@Danas-MacBook-Pro-2.local>
2024-11-26 11:11:22 -0500

    Init and Render commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 /*----- constants -----*/
 
 /*----- state variables -----*/
-
+let turn;
+let scores;
+let winner;
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+init();
+
+function init() {
+  render();
+}
+
+function render() {}
